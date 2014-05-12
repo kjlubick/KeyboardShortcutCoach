a662aa3e5ca7a2ae93eafcfb2427902bf09da2ce
Kevin Lubick <kjlubick@ncsu.edu>
2014-05-12 15:49:08 -0400

    Initial fork from KeyRocketFromGmail
    Removed Google Analytics and persistent info, as that will get reworked.
    
    Brainstorming ways to refactor it to support multiple web pages

---
+++ coach-gmail.js
@@ -0,0 +1,78 @@
+﻿/*global */
+/*
+The MIT License (MIT)
+
+Copyright (c) 2014 Kevin Lubick
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+function processAriaLabel(t) {
+    //returns own, or parent's or grand-parent's or empty aria-label  
+    al = t.attr('aria-label');
+    if (typeof al === "undefined") {
+        al = t.parent().attr('aria-label');
+        if (typeof al === "undefined") {
+            al = t.parent().parent().attr('aria-label');
+            if (typeof al === "undefined") { al = ''; }
+        }
+    }
+    t.processedAriaLabel = al;
+    cw(al);
+}
+function processText(t) {
+    // remove E-Mail Count strips _(4) from 'Inbox (4)' and (45)_ from '(45) Inbox' in right-to-left languages
+    // zero or 1 whitespace, any digits in brackets and zero or one whitespace
+    text = t.text().replace(/\W?\([\d]*\)\W?/g, "");
+    t.processedText = text;
+}
+
+
+function fillGMailShortcuts() {
+	shortcuts.push(createShortcut('t.processedText == "COMPOSE"', ['c'], 'to <u>C</u>ompose a message.'));
+	shortcuts.push(createShortcut('t.processedText == "Inbox"', ['g i'], 'to <u>G</u>o to <u>I</u>nbox.'));
+	shortcuts.push(createShortcut('t.processedText == "Starred" && (t.is("a") || t.find("a").length > 0)', ['g s'], 'to Go to Starred conversations'));
+	shortcuts.push(createShortcut('t.processedText == "Sent Mail"', ['g t'], 'to Go to Sen<u>t</u> messages'));
+	shortcuts.push(createShortcut('t.processedText == "Drafts"', ['g d'], 'to Go to Drafts'));
+	shortcuts.push(createShortcut('t.processedText == "Contacts"', ['g c'], 'to Go to Contacts'));
+	shortcuts.push(createShortcut('t.processedText == "Tasks"', ['g k'], 'to Go to Tasks'));
+	shortcuts.push(createShortcut('t.processedText == "All"', ['* a'], 'to Select all conversations'));
+	shortcuts.push(createShortcut('t.processedText == "None"', ['* n'], 'to Deselect all conversations'));
+	shortcuts.push(createShortcut('t.processedText == "Read"', ['* r'], 'to Select read conversations'));
+	shortcuts.push(createShortcut('t.processedText == "Unread"', ['* u'], 'to Select unread conversations'));
+	shortcuts.push(createShortcut('t.processedText == "Starred" && (!t.is("a") && !t.find("a").length)', ['* s'], 'to Select starred conversations'));
+	shortcuts.push(createShortcut('t.processedText == "Unstarred"', ['* t'], 'to Select unstarred conversations'));
+	shortcuts.push(createShortcut('t.processedText == "" && -1 !=t.processedAriaLabel.indexOf("Back to ")', ['u'], 'to return to conversation list'));
+	shortcuts.push(createShortcut('t.processedText == "" && -1 !=t.processedAriaLabel.indexOf("Newer")', ['k'], 'for "Newer conversation"'));
+	shortcuts.push(createShortcut('t.processedText == "" && -1 !=t.processedAriaLabel.indexOf("Older")', ['j'], 'for "Older conversation"'));
+	shortcuts.push(createShortcut('t.processedText == "" && t.attr("name") == "q"', ['/'], 'to Search email'));
+	shortcuts.push(createShortcut('t.processedText == "" && t.attr("label") == "Search people..."', ['q'], 'to search chat contacts.'));
+	shortcuts.push(createShortcut('t.processedText == "" && t.parent().attr("role")=="checkbox" && -1==t.processedAriaLabel.indexOf("tar")', ['x'], 'to (un)select conversation'));
+	shortcuts.push(createShortcut('t.processedAriaLabel == "Archive"', ['e'], 'to Archive'));
+	shortcuts.push(createShortcut('t.processedText == "Mute" || t.processedText == "Unmute"', ['m'], 'to Mute/Ignore conversation'));
+	shortcuts.push(createShortcut('t.processedAriaLabel == "Report spam" || t.processedText == "Report spam"', ['!'], 'to Report as spam'));
+	shortcuts.push(createShortcut('t.processedAriaLabel == "Delete"', ['#'], 'to move conversation to trash'));
+	shortcuts.push(createShortcut('t.processedAriaLabel == "Reply" || t.processedText == "Reply"', ['r'], 'to Reply'));
+	shortcuts.push(createShortcut('t.processedAriaLabel == "Reply to all" || t.processedText == "Reply to all"', ['a'], 'to Reply all'));
+	shortcuts.push(createShortcut('t.processedText == "Forward"', ['f'], 'to Forward the email'));
+	shortcuts.push(createShortcut('t.processedText == "Update conversation"', ['<Shift> + n'], 'to Update conversation'));
+	shortcuts.push(createShortcut('t.processedText == "Send"', ['(Tab) Enter'], 'to Send mail'));
+	shortcuts.push(createShortcut('t.processedText == "Undo"', ['z'], 'to Undo last action'));
+	shortcuts.push(createShortcut('t.processedText == "Mark as read"', ['(Shift) + i'], 'to Mark as read'));
+	shortcuts.push(createShortcut('t.processedText == "Mark as unread"', ['(Shift) + u'], 'to Mark as unread'));
+	shortcuts.push(createShortcut('t.processedText == "Mark unread from here"', ['_'], 'to Mark unread from the selected message'));
+	shortcuts.push(createShortcut('t.processedAriaLabel == "Not important"', ['+', '='], 'to Mark as important'));
+	shortcuts.push(createShortcut('-1 != t.processedAriaLabel.indexOf("Important mainly because")', ['-'], 'to Mark as not important'));
+	shortcuts.push(createShortcut('t.processedText == "Call phone"', ['g p'], 'to Make a phone call'));
+}
+
+
+//Code that runs on start
+cw("Hello startup");
+clickTargetProcessors.push(processText);
+clickTargetProcessors.push(processAriaLabel);
+fillGMailShortcuts();
---
+++ coach-utils.js
@@ -0,0 +1,176 @@
+﻿/*The MIT License (MIT)
+
+Copyright (c) 2014 Kevin Lubick
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+var timeout = 3000; // default timeout for notifications
+var debug = true; // is this a debug run?
+var supportedLanguages = ['en-GB', 'en-US', 'iw'];
+var clickTargetProcessors = new Array();
+var shortcuts = [];
+var currentNotifications = [];
+
+function cw(s) { //write to console if in debug
+    if (debug) {
+        console.log("[KR]" + s);
+    }
+    return true;
+}
+function e(s) {     //a simple function to return an empty string if object is undefined or not a string
+    if (typeof s != "string") {
+        s = '';
+    }
+    return s;
+}
+function closePopUp(p) {
+    p.fadeOut('slow');
+}
+function notify(message, type, timeout) {
+    //http://notify.dconnell.co.uk/
+    // default values
+    var markup;
+    message = typeof message !== 'undefined' ? message : 'Hello!';
+    type = typeof type !== 'undefined' ? type : 'success';
+    timeout = typeof timeout !== 'undefined' ? timeout : 3000;
+
+    // append markup if it doesn't already exist
+    if ($('#notification').length < 1) {
+        markup = '<div id="notification" class="' + type + '"><div class="kr_main">Hello!</div><div class="kr_sidemenu"><a class="close" href="#"></a></div></div>';
+
+        $('body').append(markup);
+    }
+
+    // elements
+    $notification = $('#notification');
+    $notificationSpan = $('#notification div.kr_main');
+    $notificationClose = $('#notification a.close');
+
+    // set the message
+    $notificationSpan.html(message);
+
+    // setup click event
+    $notificationClose.click(function (e) {
+        e.preventDefault();
+        $notification.stop();
+        closePopUp($notification);
+    });
+
+    // for ie6, scroll to the top first
+    if ($.browser.msie && $.browser.version < 7) {
+        $('html').scrollTop(0);
+    }
+
+    // hide old notification, then show the new notification
+    $notification.stop().removeClass().addClass(type).fadeIn('fast', function () {
+        $notification.delay(timeout);
+        closePopUp($notification);
+    });
+}
+
+function createShortcut(gE, s, m) {
+    var result = {};
+    //instead of clicking this gui elements
+    result.guiElements = gE;
+    // press these shortcuts
+    result.shortcuts = s; // for example ['g','i']
+    // to achieve this
+    result.message = m; // 
+    cw('Creating Shortcut');
+    cw('Gui Elements:' + result.guiElements);
+    cw('Shortcuts:' + result.shortcuts.join(' then '));
+    cw('Message:' + result.message);
+    return result;
+}
+
+function notify_missed_shortcut(s) {
+    var text, shortcutLabels, i, n;
+    text = "Press ";
+    shortcutLabels = s.shortcuts.slice(); //to make an independent copy
+    if (shortcutLabels.length > 0) {
+        n = shortcutLabels.length;
+        for (i = 0; i < n; i++) {
+            shortcutLabels[i] = '<span class="key">' + shortcutLabels[i].toUpperCase().replace(' ', '</span> then <span class="key">') + '</span>';
+            if (i % 2 == 1) {
+                shortcutLabels.splice(i, 0, ' or ');
+            }
+        }
+    }
+    text += shortcutLabels.join(' ');
+    text += ' ' + s.message;
+    notify(text, 'information', timeout);
+}
+
+function click(e) {
+    // http://stackoverflow.com/questions/9424550/how-can-i-detect-keyboard-events-in-gmail
+
+    var t = $(e.target);
+
+    //before the target is passed on, it is pre procesed through
+    //the array of functions in clickTargetProcessors
+    //this creates t.processedAriaLabel and t.processedText
+    $.each(clickTargetProcessors, function (i, f) { f(t); });
+
+    cw('Click t (Target):');
+    cw(t);
+    cw('t.processedText `' + t.processedText + '`');
+    cw('t.text `' + t.text() + '`');
+    cw('t.processedAriaLabel `' + t.processedAriaLabel + '`');
+    cw('t.attr("title") `' + t.attr('title') + '`');
+    //iterate through all conditions
+    $.each(shortcuts, function (i, s) {
+        var result = eval(s.guiElements);
+        //if condition was met notify and break
+        //cw(i + ' - Evaling: ' + s.guiElements + ' = ' + result);
+        if (result) {
+
+            notify_missed_shortcut(s);
+            return false;
+        }
+    });
+}
+
+(function checkForNewIframe(doc, uniq) {
+    var iframes, i, contentDocument;
+    cw("Checking for new IFrame (" + doc + "," + uniq + ")");
+    try {
+        if (!doc) {
+            return; // document does not exist. 
+        }
+
+        // Unique variable to make sure that we're not binding multiple times
+        if (!doc.rwEventsAdded73212312) {
+            //doc.addEventListener('keydown', keyDown, true);
+            //doc.addEventListener('keydown', keyUp, true);
+            doc.addEventListener('click', click, true);
+            //Mousetrap.bindEventsTo(doc);
+            doc.rwEventsAdded73212312 = uniq;
+        } else if (doc.rwEventsAdded73212312 !== uniq) {
+            // Conflict: Another script of the same type is running
+            // Do not make further calls.
+            return;
+        }
+
+        iframes = doc.getElementsByTagName('iframe');
+        for (i = 0; i < iframes.length; i++) {
+            //prevent 'Unsafe Javascript cross domain' warning http://stackoverflow.com/questions/11569723/chrome-extension-unsafe-javascript-attempt-to-access-frame-with-url-domains-pr/11570012#11570012
+            if (iframes[i].src.indexOf(location.protocol + '//' + location.host) === 0 ||
+                iframes[i].src.indexOf('about:blank') === 0 || iframes[i].src === '') {
+
+                contentDocument = iframes[i].contentDocument;
+                if (iframes[i].id == 'canvas_frame' && contentDocument && !contentDocument.rwEventsAdded73212312) {
+                    // add poller to the new iframe
+                    checkForNewIframe(iframes[i].contentDocument);
+                }
+            }
+        }
+    } catch (e) {
+        //Error: Possibly a frame from another domain?
+        cw('[ERROR] checkForNewIframe: ' + e);
+    }
+    checkForNewIframe();
+})(document, 1 + Math.random()); // Initiate recursive function for the document.
