761bc1e0f86b0380cb0930709ed13895b065150d
Kevin Lubick <kjlubick@ncsu.edu>
2014-07-03 23:32:02 -0400

    Turning stored data to Highchart-friendly format

--- source/js/stats.js
+++ source/js/stats.js
@@ -3,6 +3,12 @@
 var sampleData = [['Downloads', 4064],['Website visits', 15654],['Requested price list', 1987],['Invoice sent', 976],['Finalized', 846]];
 //var sampleData = [{"createIssue": 30}, {"goBackToIssue": 20}, {"issue": 15}, {"other": 12}];
 
+
+//var usageData = {Application1 : {keyTools: [[tool1,uses],[tool2, uses]] guiTools: [[tool3,uses],[tool4,uses]]}}
+var usageData = {};
+
+
+
 function drawGraph(element, title, data) {
 	element.highcharts({
 			chart: {
@@ -33,7 +39,7 @@ function drawGraph(element, title, data) {
 			},
 			tooltip: {
 				formatter: function() {
-					console.log(this);
+					//console.log(this);
 					return "You've invoked " + this.key +" "+ this.y+" times.";
 				}
 			},
@@ -44,19 +50,37 @@ function drawGraph(element, title, data) {
 		});
 }
 
+
+function sortArrayDoubles(a,b) {
+	return b[1] - a[1];
+}
+
 $(document).ready(function() {
 	chrome.storage.local.get(null, function(item){
 		//$("#out").text(JSON.stringify(item));
 		console.log(item);
 
-		var keyData = [];
-
 		for (var value in item) {
-			console.log(value +" : "+ item[value]);
+			var split = value.split(".");
+			var application = split[0], type = split[1], tool = split[2];
+
+			var applicationHash = usageData[application];
+			if (applicationHash === undefined) {
+				applicationHash = {key: [], menu: []};
+			} 
+			applicationHash[type].push([tool, item[value]]);
+			usageData[application] = applicationHash;
 		}
 
-		drawGraph($('#gui-graph'), "Tools invoked with Mouse", sampleData);
-		drawGraph($('#key-graph'), "Tools invoked with Keyboard Shortcuts", sampleData);
+		for(value in usageData) {
+			usageData[value].menu.sort(sortArrayDoubles);
+			usageData[value].key.sort(sortArrayDoubles);
+		}
+
+		console.log(usageData);
+
+		drawGraph($('#gui-graph'), "Tools invoked with Mouse", usageData.Gmail.menu);
+		drawGraph($('#key-graph'), "Tools invoked with Keyboard Shortcuts", usageData.Gmail.key);
 
 	});
 });
