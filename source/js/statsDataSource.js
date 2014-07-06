14a495855cf34e83e3bb9f01deaa4f0a8e11e8a5
Kevin Lubick <kjlubick@ncsu.edu>
2014-07-05 20:04:44 -0400

    Made Gmail description stubs
    Regex magic

--- source/js/statsDataSource.js
+++ source/js/statsDataSource.js
@@ -8,52 +8,158 @@ var descriptions = {"Gmail" : {
 	"archive": {
 		"long_desc": "Archive an Email",
 		"short_desc": "Archive",
-	}
+	},
+	"composeMessage": {
+		"long_desc": "Archive an Email",
+		"short_desc": "Archive",
+	},
 
-// 	"composeMessage"
-// "gotoStarred"
-// "gotoSent"
-// "gotoDrafts"
-// "gotoContacts"
-// "gotoTasks"
-// "selectAllConvos"
-// "deselectAllConvos"
-// "selectReadConvos"
-// "selectUnreadConvos"
-// "selectStarred"
-// "selectUnstarred"
-// "returnToList"
-// "newerConvo"
-// "search"
-// "searchContacts"
-// "selectSingleConvo"
-// "starConvo"
-// "archive"
-// "muteConvo"
-// "reportSpam"
-// "delete"
-// "reply"
-// "replyAll"
-// "forward"
-// "updateConvo"
-// "send"
-// "undo"
-// "markAsRead"
-// "markAsUnread"
-// "markCurrentAsUnread"
-// "markImportant"
-// "markUnimportant"
-// "makePhoneCall"
-// "searchLabel"
+
+	"gotoStarred": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"gotoSent": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"gotoDrafts": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"gotoContacts": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"gotoTasks": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"selectAllConvos": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"deselectAllConvos": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"selectReadConvos": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"selectUnreadConvos": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"selectStarred": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"selectUnstarred": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"returnToList": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"newerConvo": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"search": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"searchContacts": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"selectSingleConvo": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"starConvo": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"archive": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"muteConvo": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"reportSpam": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"delete": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"reply": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"replyAll": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"forward": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"updateConvo": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"send": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"undo": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"markAsRead": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"markAsUnread": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"markCurrentAsUnread": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"markImportant": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"markUnimportant": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"makePhoneCall": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	},
+	"searchLabel": {
+		"long_desc": "Long",
+		"short_desc": "Short"
+	}
 },
-	"GitHub" : {
+"GitHub" : {
 
-	}
+}
 };
 
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.getDescriptions)
-      sendResponse(descriptions);
-  });
+	function(request, sender, sendResponse) {
+		if (request.getDescriptions)
+			sendResponse(descriptions);
+	});
